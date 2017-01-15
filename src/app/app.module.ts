b5cc66ded15bf1a9c6f8feab3858e451d331f493
Eduardo Araujo <eduardo.araujo0@outlook.com>
2017-01-15 18:14:14 -0200

    Adicionado o módulo Routing e os componentes list e add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+/* Módulos */
+import { BrowserModule }	from '@angular/platform-browser';
+import { NgModule }			from '@angular/core';
+import { FormsModule }		from '@angular/forms';
+import { HttpModule }		from '@angular/http';
+import { RoutingModule }	from './routing/routing.module';
 
-import { AppComponent } from './app.component';
+/* Componentes */
+import { AppComponent }		from './app.component';
+import { ListComponent }	from './list/list.component';
+import { AddComponent }		from './add/add.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+    	ListComponent,
+    	AddComponent
+  	],
+  	imports: [
+    	BrowserModule,
+    	FormsModule,
+    	HttpModule,
+		RoutingModule
+  	],
+  	providers: [],
+  	bootstrap: [AppComponent]
 })
 export class AppModule { }
